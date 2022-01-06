c4eaf17bb809fa5f910b9d916c9f466895236627
magotts <techybet@gmail.com>
2022-01-05 23:46:27 -0500

    add outputMessage

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,33 @@
+const chatForm = document.getElementById("chat-form");
+
 const socket = io();
 
+
+// message from server
 socket.on("message", message => {
   console.log(message);
+  outputMessage(message);
+
 })
+
+// message submit
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // get message text
+  const msg = e.target.element.msg.value;
+
+  // emit message to server
+  socket.emit("chatMessage", msg);
+});
+
+// output message to DOM
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+  <p class="text">
+    ${message}
+  </p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
