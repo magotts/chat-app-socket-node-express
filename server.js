0d031accfef934ed3395c8ec24ab09633c91454b
magotts <techybet@gmail.com>
2022-01-06 01:10:04 -0500

    add username

--- server.js
+++ server.js
@@ -13,36 +13,37 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const botName = "ChatBot";
 // run when client connects
-io.on("connection", socket => {
-  socket.on("joinRoom", ({username, room}) => {
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
     const user = userJoin(socket.id, username, room);
     socket.join(user.room);
 
     console.log("New WS Connection...");
 
     socket.emit("message", formatMessage(botName, "Welcome to the chat!"));
-  
-  
+
     // broadcast when a user connects
-    socket.broadcast.to(user.room).emit("message", formatMessage(botName, `${user.username} has joined the chat`));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
   });
- 
 
+  // listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
 
-    // listen for chatMessage
-    socket.on("chatMessage", msg => {
-      
-      const user = getCurrentUser(socket.id);
-
-      io.to(user.room).emit("message", formatMessage(user.username, msg));
-    })
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
 
   // runs when client disconnects
   socket.on("disconnect", () => {
     // send message to all client
     io.emit("message", formatMessage(botName, "A user has left the chat"));
-  })
-})
+  });
+});
 
 const PORT = 3000 || process.env.PORT;
 
